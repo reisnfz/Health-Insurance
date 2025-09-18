322b7937bb0893e9ac9f040e39c196f649c60e58
reisnfz <briellezabala31@gmail.com>
2025-09-18 08:15:01 +0800

    Delete ClassLibraries/Health_Insurance directory
    updated

--- ClassLibraries/Health_Insurance/Health_Insurance.Domain/Entities/InsuranceRequest/IPremiumCalculateService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Health_Insurance.Domain.Entities;
-public interface IPremiumCalculateService
-{
-   Task<decimal> CalculateTotalPremiumAsync(decimal capital, IEnumerable<byte> requestCoverages,
-       CancellationToken cancellationToken = default);
-}
--- ClassLibraries/Health_Insurance/Health_Insurance.Domain/Entities/InsuranceRequest/Models/GetIncuranceRequestParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿using Health_Insurance.Domain.Entities.Base;
-
-namespace Health_Insurance.Domain.Entities;
-public class GetInsuranceRequestParameters:BaseSearchParameters
-{
-    public long? Id { get; set; }
-    public string? Title { get; set; }
-    public decimal? Capital { get; set; }
-}
